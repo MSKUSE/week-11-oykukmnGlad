7ab892c83f9c02b7444969e48ffeffa8fc741d7f
oykukmnGlad <oykukaman@posta.mu.edu.tr>
2025-05-22 12:55:42 +0300

    week 11

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,52 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
+public class Main {
+    public static void main(String[] args) throws MyEmptyStackException {
+
+        /*
+        ArrayList<Object> stringArrayList = new ArrayList();
+
+
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("String");
+
+        HashMap<String, Double> grades = new HashMap<>();
+        grades.put("Eylül",100.0);
+        System.out.println(grades);
+        grades.put("Öykü",100.0);
+        System.out.println(grades);
+        grades.put("Azra",100.0);
+        System.out.println(grades);
+        grades.put("Deniz",100.0);
+        System.out.println(grades);
+        grades.put("Mete",100.0);
+        System.out.println(grades);
+        grades.put("Mete",60.0);
+        System.out.println(grades);
+
+        System.out.println(grades.keySet());
+        System.out.println(grades.values());
+        System.out.println(grades.keySet() instanceof List);
+        System.out.println(grades.keySet().getClass());
+        System.out.println(grades.keySet() instanceof Set);
+         */
+
+        // Stack stack = new StackArray();
         Stack stack = new StackLinkedList();
-        stack.push("This is a string");
-        stack.push(5);
-        stack.peek();
-        System.out.println("Is empty "
-        +stack.isEmpty() );
-        System.out.println("Pop = " +
-                stack.pop());
-        stack.peek();
+        // stack.pop();
+        try{
+            stack.pop();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
+
+
+
+
     }
 }
